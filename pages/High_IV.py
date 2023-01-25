c80a369b81caf6e8633f7b0e3a2a33f0ac13eae6
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2023-01-25 13:53:22 +0200

    Delete High_IV.py

--- pages/High_IV.py
+++
@@ -1,9 +0,0 @@
-from openbb_terminal.sdk import openbb as obb
-import pandas as pd
-import numpy as np
-import streamlit as st
-
-st.set_page_config(page_title="High IV Scanner",)
-data = obb.stocks.options.screen.screener_output(preset='high_IV.ini')[0]
-high_iv = data.iloc[:,1:10]
-high_iv
