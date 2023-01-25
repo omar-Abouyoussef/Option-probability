43089356b721c31088aedeaec082f4fc2991d962
OK3162022 <o.abouyoussef73@gmail.com>
2023-01-25 13:48:34 +0200

    update

---
+++ pages/2_High_IV.py
@@ -0,0 +1,14 @@
+from openbb_terminal.sdk import openbb as obb
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+
+
+st.set_page_config(page_title="High IV Scanner")
+st.title("High Implied volatility Scanner")
+st.sidebar.header("High Implied volatility Scanner")
+
+data = obb.stocks.options.screen.screener_output(preset='high_IV.ini')[0]
+high_iv = data.iloc[:,1:10]
+st.write(high_iv)
